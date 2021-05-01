b98811631c20a2c41f3a3e2e6e6e09b5ed63e5c6
lizzard <lizzard@DESKTOP-L78VCEU>
2021-05-01 19:35:03 +0300

    1) Вывод титульного bmp 2) Реализован выход из СДЛ пользователем

--- Term_Paper/Navigation.cpp
+++ Term_Paper/Navigation.cpp
@@ -1,29 +1,91 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include "SDL.h"
 #include "Navigation.h"
+
+/*
+	number_of_picture:
+	1 - titul list
+*/
 
 struct Pointer_On
 {
 	SDL_Window* Window;
 	SDL_Surface* Window_Surface;
-	SDL_Surface* Background_Image;
+	SDL_Surface* Screen_Surface;
+	SDL_Surface* Image;
 };
 
-
+//loading image in background
+/*
+	number_of_picture:
+	1 - titul list
+	2 - main menu
+*/
 void Load_Backgroung_Image(struct Pointer_On* Pointer_On, int variant_of_background_image)
 {
-	Pointer_On->Background_Image = SDL_GetWindowSurface(Pointer_On->Window);
-	Pointer_On->Background_Image = SDL_LoadBMP("title_image.bmp");
-	SDL_UpdateWindowSurface(Pointer_On->Window);
+	SDL_Rect dest;
+	dest.x = 0;
+	dest.y = 0;
+	switch (variant_of_background_image)
+	{
+		case 1:
+		{
+			Pointer_On->Screen_Surface = SDL_GetWindowSurface(Pointer_On->Window);
+			Pointer_On->Image = SDL_LoadBMP("title_image.bmp");
+			SDL_BlitSurface(Pointer_On->Image, NULL, Pointer_On->Screen_Surface, &dest);
+			SDL_UpdateWindowSurface(Pointer_On->Window);
+			break;
+		}
+		case 2:
+		{
+			Pointer_On->Screen_Surface = SDL_GetWindowSurface(Pointer_On->Window);
+			Pointer_On->Image = SDL_LoadBMP("main_menu.bmp");
+			SDL_BlitSurface(Pointer_On->Image, NULL, Pointer_On->Screen_Surface, &dest);
+			SDL_UpdateWindowSurface(Pointer_On->Window);
+			break;
+		}
+		default:
+		{
+			break;
+		}
+	}
 }
 
+//making navigation
 void Navigation()
 {
+	/*
+		number_of_picture:
+		1 - titul list
+		2 - main menu
+	*/
+	int number_of_picture = 1;
+	bool Quit_From_Programm = false;
 	struct Pointer_On Pointer_On;
 	Pointer_On.Window = SDL_CreateWindow("Curs Project",
 		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600,
 		600, SDL_WINDOW_SHOWN);
-	SDL_Delay(1000);
-	SDL_DestroyWindow(Pointer_On.Window);
-	SDL_Quit();
+	SDL_Event user_click;
+	Load_Backgroung_Image(&Pointer_On, number_of_picture);
+	while (!Quit_From_Programm)
+	{
+		SDL_PollEvent(&user_click);
+		if (user_click.button.button == SDL_BUTTON_LEFT 
+			&& user_click.button.x >= 237 && user_click.button.x <= 361
+			&& user_click.button.y >= 371 && user_click.button.y <= 300
+			&& number_of_picture == 1)
+		{
+			number_of_picture = 2;//main menu
+			Load_Backgroung_Image(&Pointer_On, number_of_picture);
+		}
+		/*if (user_click.button.button == SDL_BUTTON_LEFT
+			&& user_click.button.x >= 244 && user_click.button.x <= 355
+			&& user_click.button.y >= 498 && user_click.button.y <= 543
+			&& number_of_picture == 1)
+		{
+			SDL_DestroyWindow(Pointer_On.Window);
+			SDL_Quit();
+		}*/
+	}
 }
