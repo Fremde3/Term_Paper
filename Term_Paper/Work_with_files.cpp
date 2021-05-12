103bf0d07f72651c934f55955cf98281663aafd9
Joker <Joker@DESKTOP-GR8B8OJ>
2021-05-12 22:22:56 +0300

    1) Добавлены файлы для фунций работы с текстовыми файлами 2) Функции пока спейсхолдеры

---
+++ Term_Paper/Work_with_files.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "Navigation.h"
+#include "Demo_Move.h"
+
+void save_position_game_1()
+{
+
+}
+
+void save_position_game_2()
+{
+
+}
+
+void put_position_from_file_game_1()
+{
+
+}
+
+void put_position_from_file_game_2()
+{
+
+}
---
+++ Term_Paper/Work_with_files.h
@@ -0,0 +1,9 @@
+#pragma once
+
+void save_position_game_1();
+
+void save_position_game_2();
+
+void put_position_from_file_game_1();
+
+void put_position_from_file_game_2();
